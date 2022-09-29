35a6e46b28619cf47228987fbc4e8341e217ddfa
ismail-miah <miahismail654@gmail.com>
2022-09-29 19:14:29 +0600

    redux | Reducers

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 // defining constants
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
-const ADD_USER = "ADD-USER";
 
 //state
 const initialCounterState = {
   count: 0,
-};
-const initialUsersState = {
-  users: [{ name: "anisul islam" }],
 };
 
 //action
@@ -23,9 +19,23 @@ const decrementCounter = () => {
     type: DECREMENT,
   };
 };
-const addUser = (usr) => {
-  return {
-    type: ADD_USER,
-    payload: { name: "shakil" },
-  };
+
+// create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    default:
+      state;
+  }
 };
