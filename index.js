879f9149424cb3ef90343073e60fe73a3d503b63
ismail-miah <miahismail654@gmail.com>
2022-10-01 10:37:05 +0600

    multiple reducer error

--- index.js
+++ index.js
@@ -4,20 +4,10 @@ const { createStore, combineReducers } = require("redux");
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCT = "ADD_PRODUCT";
 
-//Cart Constants
-const GET_CART_ITEMS = "GET_CART_ITEMS";
-const ADD_CART_ITEM = "ADD_CART_ITEM";
-
 //product STATE
 const initialProductsState = {
   products: ["sugar", "salt"],
   numberOfProducts: 2,
-};
-
-//CART INITIALSTATE
-const initialCartState = {
-  products: ["sugar"],
-  numberOfProducts: 1,
 };
 
 // PRODUCT ACTION
@@ -29,19 +19,6 @@ const getProducts = () => {
 const addProduct = (product) => {
   return {
     type: ADD_PRODUCT,
-    payload: product,
-  };
-};
-
-// CART  ACTION
-const getCart = () => {
-  return {
-    type: GET_CART_ITEMS,
-  };
-};
-const addCart = (product) => {
-  return {
-    type: ADD_CART_ITEM,
     payload: product,
   };
 };
@@ -65,40 +42,10 @@ const productReducer = (state = initialProductsState, action) => {
   }
 };
 
-// Cart Reducer
-
-const cartReducer = (state = initialCartState, action) => {
-  switch (action.type) {
-    case GET_CART_ITEMS:
-      return {
-        ...state,
-      };
-    case ADD_CART_ITEM:
-      return {
-        products: [...state.products, action.payload],
-        numberOfProducts: state.numberOfProducts + 1,
-      };
-
-    default:
-      return state;
-  }
-};
-
 // combineReducerStore
 
-const rootReducer = combineReducers({
-  productR: productReducer,
-  cartR: cartReducer,
-});
-
 //store
-const store = createStore(rootReducer);
+const store = createStore(productReducer);
 store.subscribe(() => {
   console.log(store.getState());
 });
-
-store.dispatch(getProducts());
-store.dispatch(addProduct("pen"));
-
-store.dispatch(getCart());
-store.dispatch(addCart("pen"));
